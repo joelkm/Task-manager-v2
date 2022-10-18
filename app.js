ccb9c5e62dc7f081b2dd8a37c235255152b258f9
JoelKM <joelkasmor16@gmail.com>
2022-10-18 14:45:57 +0200

    form features

--- app.js
+++ app.js
@@ -1,22 +1,59 @@
-class Task extends HTMLElement{
+const taskList = document.getElementById('taskList');
+const add = document.getElementById('addTask');
+const menu = document.getElementById('taskCreation');
+const msg = document.getElementById('msg');
+
+
+
+/*class Task extends HTMLElement{
 
     constructor (){
         super()
     }
     
     connectedCallback(){
-        this.innerHTML="<div class='task'><h3>title</h3><div class=managbuttons><button id class= "button">+</button></div></div>"
+        this.innerHTML=`
+        <div class='task'>
+        <h3>title</h3>
+        <div class=managbuttons>
+        <button id class= "button">+</button>
+        </div>
+        </div>
+        `
     }
 }
 
 window.customElements.define('task-element', Task);
+*/
 
-const taskList = document.getElementById('taskList');
-const add = document.getElementById('addTask');
 
 add.addEventListener('click', newTask);
 
 function newTask(){
-    taskList.innerHTML += '<task-element></task-element>';
     console.log('click');
+    menu.style.visibility= 'visible';
 }
+
+menu.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log("post request");
+    formValidation();
+});
+
+function formValidation(){
+    if (input.value==="") {
+        msg.innerHTML = "Post cannot be blank";
+        console.log("failure");
+    }
+    else{
+        msg.innerHTML = "";
+        acceptData();
+        console.log("successs");
+    }
+}
+/*
+function acceptData(){
+    data["text"] = .value;
+    console.log(data);
+    createTask();
+}*/
