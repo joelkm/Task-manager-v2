76bcd011ec8f4ea11194b589f5768ee6ad407647
JoelKM <joelkasmor16@gmail.com>
2023-01-25 10:30:59 +0100

    setup task model

--- server/src/models/tasks/tasks.model.js
+++ server/src/models/tasks/tasks.model.js
@@ -1,28 +1,65 @@
-import { find, create, deleteOne } from './users.mongo';
+import { find, create, findByIdAndUpdate, deleteOne } from './users.mongo';
 
-async function getUserById(userId) {
-    const user = await find({username}, '_id username') 
-    if (user) {
-        return user;
+async function getTasksFromUserId (userId) {
+    const tasks = await find({user: userId}, '_id name description when important') 
+    if (!tasks) {
+        return false;
     }
-    else {
-        return false
-    }
-} 
+    return tasks;
+}
 
-async function addNewUser(user) {
+async function addNewTask (task) {
     try {
-        const newUser = await create({
-            name: user.username
+        const newTask = create({
+            userId: task.userId,
+            name: task.name,
+            description: task.description,
+            when: task.when,
+            important: task.important
         })
-        return newUser;
+        return newTask;
+    } catch (err) {
+        console.error(`Could not save the task: ${err}`)
+        return false;
+    }
+}
+
+async function editTaskById (taskId, newTaskInfo) {
+    try {
+        const nonEditedTask = await find({_id: taskId});
+        const editedTask = await findByIdAndUpdate({_id: taskId},{"name": newTaskInfo.name, "description": newTaskInfo.description, "userId": newTaskInfo.userId});
+
+        if(nonEditedTask==editedTask){
+            console.error('Task was not updated');
+            return false; 
+        }
+        return editedBlog;
+    } catch (err) {
+        console.error(`Could not update task: ${err}`);
+        return false;
+    }
+}
+
+async function deleteTaskById (taskId) {
+    try {
+        const task = find({_id: taskId})
+        const deleted = await deleteOne({_id: taskId});
+
+        if(deleted.deletedCount == 0) {
+            console.error('Nothing was deleted')
+            return false;
+        }
+
+        return task;
     } catch(err) {
-        console.error(`Could not add user: ${err}`);
+        console.error(`Could not delete the task: ${err}`);
         return false;
     }
 }
 
 export default {
-    getUserById,
-    addNewUser
+    getTasksFromUserId,
+    addNewTask,
+    editTaskById,
+    deleteTaskById
 }
