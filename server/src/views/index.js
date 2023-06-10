f095370a94f34fbff56a6bb631593f6101124e78
Joel Kassim Moratalla <joelkasmor16@gmail.com>
2023-06-10 18:30:43 +0200

    Protect views

--- server/src/views/index.js
+++ server/src/views/index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const router = express.Router();
 const path = require('path');
-
-const { checkSession, checkNoSession } = require('../common/session-status');
+const { checkLoged, checkNotLoged } = require('../common/authCheck');
 
 const distPath = path.join(__dirname, "..", "..", "..", "client", "dist");
 
 router.use(express.static(distPath, {index: false}));
 
-router.get("/*", (req, res) => {
+router.get("/", checkLoged, (req, res) => {
+    if (!req.user) res.redirect('/login') 
+    res.sendFile(path.join(distPath, "index.html"));
+});
+
+router.get("/login", (req, res) => {
+    if (req.user) res.redirect('/') 
+    res.sendFile(path.join(distPath, "index.html"));
+});
+
+router.get("/signup", (req, res) => {
+    if (req.user) res.redirect('/') 
     res.sendFile(path.join(distPath, "index.html"));
 });
 
